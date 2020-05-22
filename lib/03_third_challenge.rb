6146774584fd4146eb3fcf10d3ed56306f02480d
Alexandra Ortiz <alexpac27@gmail.com>
2020-05-22 19:05:00 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -2,10 +2,10 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        #your key/value pairs here
+        ["Lord Montague", "53"]
         },
       :matriarch => {
-        #your key/value pairs here
+        ["Lady Montague", "54"]
         },
       :hero => {
         #your key/value pairs here
